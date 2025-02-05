57c2ca33df1889ae72376585aac0a1433f332b5b
Oluwanifemi Adeyemi <deyeminifemi@gmail.com>
2025-02-05 12:02:32 +0100

    base navbar

---
+++ components/navbar.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import ModeToggleDark from "assets/icon/mode-toggle-dark.svg";
+import ModeToggleLight from "assets/icon/mode-toggle-light.svg";
+
+const colorMode = useColorMode();
+
+const isDarkMode = computed(() => colorMode.value === 'dark');
+
+const toggleColorMode = () => {
+    if(colorMode.preference === 'dark'){
+        colorMode.preference = 'light'
+    }else{
+        colorMode.preference = 'dark'
+    }
+}
+
+const links = [
+    {
+        link: "/work",
+        label: "Work"
+    },
+    {
+        link: "/about",
+        label: "About"
+    },
+    {
+        link: "/notes",
+        label: "Notes"
+    },
+    {
+        link: "/journal",
+        label: "Journal"
+    },
+]
+</script>
+
+<template>
+    <nav class="flex pt-10 px-6">
+        <div>
+            <nuxt-link to="/" class="mb-2 text-primary">
+                Oluwafemi Soetan
+            </nuxt-link>
+            <p class="text-gray">
+                Digital Designer
+            </p>
+        </div>
+        <ul class="mx-auto flex gap-x-6">
+           <li v-for="link in links" class="w-[96px]">
+               <nuxt-link :to="link.link" class="text-gray">
+                   {{link.label}}
+               </nuxt-link>
+           </li>
+        </ul>
+        <button @click="toggleColorMode" class="self-start">
+            <ModeToggleDark  width="25" height="25" filled v-if="isDarkMode"/>
+            <ModeToggleLight width="25" height="25"  filled v-else/>
+        </button>
+    </nav>
+</template>
+
+<style scoped>
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,29 +1,13 @@
 <template>
     <div :class="{dark: isDarkMode}">
         <div class="bg-background min-h-screen w-screen">
-            <button @click="toggleColorMode">
-                <ModeToggleDark filled v-if="isDarkMode"/>
-                <ModeToggleLight filled v-else/>
-            </button>
-            <slot />
-            <button @click="toggleColorMode" class="text-primary">
-                Toggle Mode
-            </button>
+            <Navbar/>
         </div>
     </div>
 </template>
 <script setup>
-import ModeToggleDark from "~/assets/icon/mode-toggle-dark.svg"
-import ModeToggleLight from "~/assets/icon/mode-toggle-light.svg"
+import Navbar from "~/components/navbar.vue";
 const colorMode = useColorMode();
 
 const isDarkMode = computed(() => colorMode.value === 'dark');
-
-const toggleColorMode = () => {
-    if(colorMode.preference === 'dark'){
-        colorMode.preference = 'light'
-    }else{
-        colorMode.preference = 'dark'
-    }
-}
 </script>
