ac93c8b8b616d6ab78065a882a54f2ee0ce826fb
Oluwanifemi Adeyemi <deyeminifemi@gmail.com>
2025-02-05 23:19:17 +0100

    fetch and render homepage content

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,26 @@
 <template>
-  <div>
-      <h1 class="text-primary text-4xl">
-            WELCOME
-      </h1>
-  </div>
+    <div class="w-[469px] mx-auto pt-7 mb-4 homepage">
+        <section class="main-text mb-6">
+            <prismic-rich-text :field="home.data.main_text"/>
+        </section>
+        <section class="flex space-x-2">
+            <a class="link" v-for="link in home.data.links" :href="link.link.url">
+                {{link.label}}
+            </a>
+        </section>
+    </div>
 </template>
+<script setup>
+    const { client } = usePrismic();
+
+    const { data: home } = await useAsyncData("home", () => client.getSingle("homepage"));
+</script>
+<style>
+.main-text a{
+    text-decoration: underline;
+}
+
+.link{
+    text-decoration: underline;
+}
+</style>
