8a8f2f2a3fd3aee36f0dc84c694939ac591b6515
Oluwanifemi Adeyemi <deyeminifemi@gmail.com>
2025-04-19 10:37:49 +0100

    tweak width of portrait visuals

--- slices/VisualMediaRow/index.vue
+++ slices/VisualMediaRow/index.vue
@@ -29,8 +29,8 @@ const hasLandscapeImageAndIsRow =
           :type="media.media.kind === 'image' ? 'image' : 'video'"
           :src="media.media.url || ''"
           :thumbnail-src="media.thumbnail?.url || ''"
-          inner-class="h-full w-full md:object-cover object-contain"
-          class="max-md:h-[240px] h-full min-w-0 >img:h-full w-fit" :class="{'md:w-[193px]': media.variant === 'Portrait', 'md:w-[360px]': media.variant === 'Landscape'}"
+          inner-class="h-full w-full object-cover"
+          class="max-md:h-[240px] h-full min-w-0 >img:h-full" :class="{'w-[193px]': media.variant === 'Portrait', 'md:w-[360px]': media.variant === 'Landscape'}"
       />
   </section>
 </template>
