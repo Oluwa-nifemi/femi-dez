c87626faf1bb67a636dae03c816fd7aff8ff0398
Oluwanifemi Adeyemi <deyeminifemi@gmail.com>
2025-04-22 16:09:48 +0100

    updated long and lat

--- components/base-layout.vue
+++ components/base-layout.vue
@@ -29,8 +29,8 @@ defineProps({
 import Navbar from "~/components/navbar.vue";
 import { format } from "date-fns";
 const { data: weatherInfo } = useAsyncData(async () => {
-	const latitude = 52.9399; // Saskatchewan, Canada
-	const longitude = 106.4509;
+	const latitude = 50.8001; // Saskatchewan, Canada
+	const longitude = -104.9344;
 	const apiUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,weathercode&timezone=auto`;
 
 	const response = await fetch(apiUrl);
